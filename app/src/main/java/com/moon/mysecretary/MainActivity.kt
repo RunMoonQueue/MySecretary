6f238689760c93ca35d303ed9301290902c3e7bc
LAPTOP-6LQBHRCU\skyli <skylife927@naver.com>
2021-12-15 23:23:22 +0900

    Splash screen 애니메이션 추가 및 main screen 이동

--- app/src/main/java/com/moon/mysecretary/MainActivity.kt
+++ app/src/main/java/com/moon/mysecretary/MainActivity.kt
@@ -3,11 +3,15 @@ package com.moon.mysecretary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -20,8 +24,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             MysecretaryTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    MainScreen()
+                Surface(color = Color.White, modifier = Modifier.fillMaxSize()) {
+                    Navigation()
                 }
             }
         }
@@ -30,17 +34,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    val navController = rememberNavController()
-    Scaffold {
-        Navigation(navController = navController)
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "Main Screen", color = Color.Black, fontSize = 24.sp)
     }
 }
 
 @Composable
-fun Navigation(navController: NavHostController) {
+fun Navigation() {
+    val navController = rememberNavController()
     NavHost(navController, startDestination = "splash_screen") {
-        composable("splash_screen"){
-            SplashScreen()
+        composable("splash_screen") {
+            SplashScreen(navController)
+        }
+        composable("main_screen") {
+            MainScreen()
         }
     }
 }
--- app/src/main/java/com/moon/mysecretary/ui/SplashScreen.kt
+++ app/src/main/java/com/moon/mysecretary/ui/SplashScreen.kt
@@ -1,10 +1,66 @@
 package com.moon.mysecretary.ui
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.moon.mysecretary.R
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen() {
-    Text(text="Splash Screen", style = MaterialTheme.typography.h6)
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
+    // Animation
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.7f,
+            // tween Animation
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                })
+        )
+        // Customize the delay time
+        delay(1000L)
+        navController.navigate("main_screen")
+    }
+
+    // Image
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Change the logo
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.ic_notification),
+                modifier = Modifier
+                    .scale(scale.value)
+                    .size(width = 64.dp, height = 64.dp),
+                contentDescription = "Logo",
+            )
+            Text(
+                text = "모두의 알람",
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.align(Alignment.CenterVertically).padding(4.dp)
+            )
+        }
+
+    }
 }
