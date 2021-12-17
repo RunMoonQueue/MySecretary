00ddc27aa29638cc1c1ab24f63992b43ef74de33
LAPTOP-6LQBHRCU\skyli <skylife927@naver.com>
2021-12-17 23:39:27 +0900

    Date, Time Picker 설정

--- app/src/main/java/com/moon/mysecretary/MainActivity.kt
+++ app/src/main/java/com/moon/mysecretary/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.moon.mysecretary
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,6 +26,10 @@ import androidx.navigation.compose.rememberNavController
 import com.moon.mysecretary.ui.SplashScreen
 import com.moon.mysecretary.ui.theme.MysecretaryTheme
 import com.moon.mysecretary.ui.theme.graySurface
+import com.vanpra.composematerialdialogs.MaterialDialog
+import com.vanpra.composematerialdialogs.datetime.date.datepicker
+import com.vanpra.composematerialdialogs.datetime.time.timepicker
+import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -77,12 +79,51 @@ fun Navigation() {
 
 @Composable
 fun EditScreen(navController: NavController) {
-    var text by rememberSaveable { mutableStateOf("Text") }
+
+    var text by remember { mutableStateOf("") }
+    val dialogDateState = rememberMaterialDialogState()
+    val dialogTimeState = rememberMaterialDialogState()
+    var dateTime by remember { mutableStateOf("") }
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Column {
-            TextField(value = "알람 이름", onValueChange = {
+            OutlinedTextField(value = text, onValueChange = {
                 text = it
+            }, label = { Text("알람 이름") })
+            MaterialDialog(dialogState = dialogTimeState,
+                buttons = {
+                    positiveButton("Complete", onClick = {
+                        dialogTimeState.hide()
+                    })
+                    negativeButton("Cancel", onClick = {
+                        dialogTimeState.hide()
+                    })
+                }) {
+                timepicker { time ->
+                    dateTime = "$dateTime $time"
+                    Log.i("MQ!", "time:$time")
+                }
+            }
+            MaterialDialog(
+                dialogState = dialogDateState,
+                buttons = {
+                    positiveButton(text = "Next", onClick = {
+                        dialogDateState.hide()
+                        dialogTimeState.show()
+                    })
+                    negativeButton(text = "Cancel", onClick = {
+                        dialogDateState.hide()
+                    })
+                }
+            ) {
+                datepicker { date ->
+                    dateTime = "$date"
+                    Log.i("MQ!", "date:$date")
+                }
+            }
+            Text(text = "시작날짜", modifier = Modifier.clickable {
+                dialogDateState.show()
             })
+            Text(text = dateTime)
         }
     }
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
